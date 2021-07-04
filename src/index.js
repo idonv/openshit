d5d36982cb052b8fd8e480f87ac831b517e5e284
NAVEH IDO <IDONAV2@rf.local>
2021-07-04 14:21:01 +0300

    fixed index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.sendFile('./pages/index.html');
+    res.sendFile(__dirname + '/pages/index.html', {  }, null);
 })
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}...`));
